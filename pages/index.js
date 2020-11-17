8c22e644d0e5d3545625a59e1cb6159e3f777018
unknown <frankenpereira@gmail.com>
2020-11-17 10:07:51 +0000

    Primeira versão da calculadora (com 2 inputs)

--- pages/index.js
+++ pages/index.js
@@ -12,12 +12,12 @@ function Contador(){
     function resetContador(){
         setContador(1)
     }
-
+    
     function somarContador(){
         var num2 = parseFloat(somar.value)
         setContador(contador + num2)
     }
-
+    
     function subtrairContador(){
         var num2 = parseFloat(somar.value)
         setContador(contador - num2)
@@ -28,26 +28,129 @@ function Contador(){
         var num2 = parseFloat(somar.value)
         setContador(contador * num2)
     }
-
+    
     function dividirContador(){
         var num2 = parseFloat(somar.value)
         setContador(contador / num2)
     }
+
+    
     return(
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-            <button onClick={resetContador}>Resetar</button>
-            <input id="somar" placeholder="Insira um numero"></input>
-            <button onClick={somarContador}>Somar</button>
-            <button onClick={subtrairContador}>Subtrair</button>
-            <button onClick={multiplicarContador}>Multiplicar</button>
-            <button onClick={dividirContador}>Dividir</button>
+        <div style={{backgroundColor: "#14A"}}>
+            <div style={{color: "#FFF", padding: "20px"}} >{contador}</div>
+            <button style={buttonStyles} onClick={adicionarContador}>Adicionar</button>
+            <button style={buttonStyles} onClick={resetContador}>Resetar</button>
+            <input style={inputStyles} id="somar" placeholder="Insira um numero"></input>
+            <button style={buttonStyles} onClick={somarContador}>Somar</button>
+            <button style={buttonStyles} onClick={subtrairContador}>Subtrair</button>
+            <button style={buttonStyles} onClick={multiplicarContador}>Multiplicar</button>
+            <button style={buttonStyles} onClick={dividirContador}>Dividir</button>
+            
         </div>
     )
 }
 
+function Resultado(){
+    const [resultado, setResultado] = useState(0)
+    // const [resultadoFinal, setResultadoFinal] = useState()
+    
+    function somarResultado(){
+        setResultado(parseFloat(num0.value) + parseFloat(num1.value))
+    }
+    function subtrairResultado(){
+        setResultado(parseFloat(num0.value) - parseFloat(num1.value))
+    }
+    function multiplicarResultado(){
+        setResultado(parseFloat(num0.value) * parseFloat(num1.value))
+    }
+    function dividirResultado(){
+        setResultado(parseFloat(num0.value) / parseFloat(num1.value))
+    }
+    function resetCalculadora(){
+        setResultado(0)
+        // setResultadoFinal(0)
+        num0.value = ""
+        num1.value = ''
+    }
+    function resetInput(){
+        num0.value = ''
+        num1.value = ''
+    }
+
+    //tela
+    return(
+        <div style={{backgroundColor: "#111"}}>
+            <div style={resultadoStyles}>{resultado}</div>
+            {/* <div style={resultadoStyles}>{resultadoFinal}</div> */}
+            <input style={inputStyles} id="num0" placeholder="Insira um numero"></input>
+            <input style={inputStyles} id="num1" placeholder="Insira um numero"></input>
+            <button style={buttonStyles} onClick={resetInput}>CE</button>
+            <button style={cButtonStyles} onClick={resetCalculadora}>C</button>
+            <br></br>
+            <button style={buttonStyles} onClick={somarResultado}>Somar</button>
+            <button style={buttonStyles} onClick={subtrairResultado}>Subtrair</button>
+            <button style={buttonStyles} onClick={multiplicarResultado}>Multiplicar</button>
+            <button style={buttonStyles} onClick={dividirResultado}>Dividir</button>
+            <br></br>
+            {/* <button style={calcularButtonStyles} onClick={calcularResultado}>Calcular</button> */}
+        </div>
+    )
+}
 function Home(){
-    return <Contador />
+    return <Contador /> && <Resultado />             
 }
 export default Home
+
+//Styles
+const buttonStyles = {
+    height: 50,
+    backgroundColor: "#121212",
+
+    margin: "20px 20px",
+
+    color: "#fff",
+
+    // border: "none",
+    borderStyle: "solid",
+    borderRadius: 10,
+    borderColor: "#888",
+
+    cursor: "pointer"
+}
+
+const cButtonStyles ={
+    height: 50,
+    width: 30,
+    backgroundColor: "#121212",
+
+    marginBottom: 20,
+
+    borderStyle: "solid",
+    borderRadius: 10,
+    borderColor: "#888",
+    
+    color: "#fff",
+
+    cursor: "pointer"    
+}
+
+const inputStyles = {
+    height: 50,
+    marginLeft: 20,
+    backgroundColor: "#121212",
+    color: "#fff",
+    borderStyle: "solid",
+    borderRadius: 10,
+    borderColor: "#888",
+    paddingLeft: "10px"
+}
+
+const resultadoStyles = {
+    padding: "10px 0px",
+    margin: "0px 20px",
+
+    color: "#fff",
+
+    fontSize: "24px",
+    fontFamily: "Arial",
+}
